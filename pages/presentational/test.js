dcd2d56a7ca26d263db887e34931bd4b4d3045ea
Jonathan Ko <jonathan.ko523@gmail.com>
2020-12-22 00:15:05 -0800

    Css setup

---
+++ pages/presentational/test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { createUseStyles } from "react-jss";
+
+export const useStyles = createUseStyles({});
+
+export function Sidebar(props) {
+  const classes = useStyles();
+
+  return <div>TEST</div>;
+}
+
+export default Sidebar;
