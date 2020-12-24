2b96fa9ec052100d64d2925c3cbda46f75f69b0b
Jonathan Ko <jonathan.ko523@gmail.com>
2020-12-23 22:16:00 -0800

    body size set

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,20 @@ import Sidebar from "./presentational/sidebar";
 import { createUseStyles } from "react-jss";
 
 export const useStyles = createUseStyles({
-  header: { border: "solid 1px" },
+  home: {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+    border: "solid 1px red",
+    margin: "-8px",
+  },
 });
 
 export default function Home() {
   const classes = useStyles();
 
   return (
-    <div className={classes.header}>
+    <div className={classes.home}>
       <Header></Header>
       <Sidebar></Sidebar>
     </div>
