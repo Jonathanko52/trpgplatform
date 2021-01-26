67066a9fb142e2b649c17d1aef7a2c2cc15a56fb
Jonathan Ko <jonathan.ko523@gmail.com>
2021-01-25 23:54:57 -0800

    breakthrough on organization

--- pages/presentational/characterSheet.js
+++ pages/presentational/characterSheet.js
@@ -10,10 +10,12 @@ export const useStyles = createUseStyles({
 export function CharacterSheet() {
   const classes = useStyles();
   let contents = [];
+  let organization = [];
   let pageData = data.characterSheet.noncalculatedValues;
   let additionalData = data.characterSheet.organization;
   for (const key in pageData.valueNames) {
     contents.push([pageData.valueNames[key], pageData[key]]);
+    organization;
   }
 
   return (
