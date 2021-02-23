b992f5a38e41eae54023d14b946cdcae34b72665
Jonathan Ko <jonathan.ko523@gmail.com>
2021-02-22 19:08:38 -0800

    putting page data into calc funcs

--- pages/presentational/characterSheet.js
+++ pages/presentational/characterSheet.js
@@ -54,7 +54,7 @@ export function CharacterSheet() {
   for (const key in calculatedValues.valueNames) {
     //runs the calculation on calculated value, which isn't terribly accurate
     let calculationFunction = data.characterSheet.calculations[key];
-    eval(calculationFunction.func)();
+    eval(calculationFunction.func)(pageData);
     // let calculatedValuesAfterCalculation = calculationFunction(pageData);
     // contents[key] = [
     //   calculatedValues.valueNames[key],
